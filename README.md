1bb10e321f3ca001394ae42347cf8a4496b46d11
Dang Xuan Truong <truongdx@runsystem.net>
2025-03-19 14:38:05 +0700

    ip

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-1 : python -m venv env to install
-2 : create file requirements.txt to library need install
-3: pip install -r requirements.txt
-use command uvicorn main:app to run project or  python -m uvicorn main:app --host 0.0.0.0 --port 8000
-python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+﻿# FastAPI Tutorial
+
+## Setup
+Set up your virtual environment
+```bash
+python -m venv env
+.\env\Scripts\activate
+pip install -r requirements.txt
+```
+If you're running Linux or MacOS you'll instead run
+```bash
+python -m venv env
+source ./env/bin/activate
+pip install -r requirements.txt
+```
+
+## Running the app
+`uvicorn main:app --reload`
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from datetime import datetime, time, timedelta
 from enum import Enum
+from typing import Literal
+from uuid import UUID
 
-from fastapi import FastAPI, Query, Body, Path
-from pydantic import BaseModel
+from fastapi import Body, FastAPI, Query, Path, Cookie, Header
+from pydantic import BaseModel, Field, HttpUrl, EmailStr
 
 app = FastAPI()
 
@@ -9,60 +12,60 @@ app = FastAPI()
 # @app.get("/")
 # async def root():
 #     return {"message": "hello world"}
-
-
+#
+#
 # @app.post("/")
 # async def post():
 #     return {"message": "hello from the post route"}
-
-
+#
+#
 # @app.put("/")
 # async def put():
 #     return {"message": "hello from the put route"}
-
-
+#
+#
 # @app.get("/users")
 # async def list_users():
 #     return {"message": "list users route"}
-
-
+#
+#
 # @app.get("/users/me")
 # async def get_current_user():
 #     return {"Message": "this is the current user"}
-
-
+#
+#
 # @app.get("/users/{user_id}")
 # async def get_user(user_id: str):
 #     return {"user_id": user_id}
-
-
+#
+#
 # class FoodEnum(str, Enum):
 #     fruits = "fruits"
 #     vegetables = "vegetables"
 #     dairy = "dairy"
-
-
+#
+#
 # @app.get("/foods/{food_name}")
 # async def get_food(food_name: FoodEnum):
 #     if food_name == FoodEnum.vegetables:
 #         return {"food_name": food_name, "message": "you are healthy"}
-
+#
 #     if food_name.value == "fruits":
 #         return {
 #             "food_name": food_name,
 #             "message": "you are still healthy, but like sweet things",
 #         }
 #     return {"food_name": food_name, "message": "i like chocolate milk"}
-
-
+#
+#
 # fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-
+#
+#
 # @app.get("/old_items")
 # async def list_old_items(skip: int = 0, limit: int = 10):
 #     return fake_items_db[skip : skip + limit]
-
-
+#
+#
 # @app.get("/items/{item_id}")
 # async def get_item(
 #     item_id: str, sample_query_param: str, q: str | None = None, short: bool = False
@@ -77,8 +80,8 @@ app = FastAPI()
 #             }
 #         )
 #     return item
-
-
+#
+#
 # @app.get("/users/{user_id}/items/{item_id}")
 # async def get_user_item(
 #     user_id: int, item_id: str, q: str | None = None, short: bool = False
@@ -93,15 +96,15 @@ app = FastAPI()
 #             }
 #         )
 #         return item
-
-
+#
+#
 # class Item(BaseModel):
 #     name: str
 #     description: str | None = None
 #     price: float
 #     tax: float | None = None
-
-
+#
+#
 # @app.post("/items")
 # async def create_item(item: Item):
 #     item_dict = item.dict()
@@ -109,16 +112,16 @@ app = FastAPI()
 #         price_with_tax = item.price + item.tax
 #         item_dict.update({"price_with_tax": price_with_tax})
 #     return item_dict
-
-
+#
+#
 # @app.put("/items/{item_id}")
 # async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
 #     result = {"item_id": item_id, **item.dict()}
 #     if q:
 #         result.update({"q": q})
 #     return result
-
-
+#
+#
 # @app.get("/items")
 # async def read_items(
 #     q: str
@@ -135,8 +138,8 @@ app = FastAPI()
 #     if q:
 #         results.update({"q": q})
 #     return results
-
-
+#
+#
 # @app.get("/items_hidden")
 # async def hidden_query_route(
 #     hidden_query: str | None = Query(None, include_in_schema=False)
@@ -144,8 +147,8 @@ app = FastAPI()
 #     if hidden_query:
 #         return {"hidden_query": hidden_query}
 #     return {"hidden_query": "Not found"}
-
-
+#
+#
 # @app.get("/items_validation/{item_id}")
 # async def read_items_validation(
 #     *,
@@ -157,40 +160,234 @@ app = FastAPI()
 #     if q:
 #         results.update({"q": q})
 #     return results
-#---
-# part 7 ->body - Multiple Parameters
-#--- 
 
+
+# Part 7 -> Body - Multiple Parameters
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#
+#
+# class User(BaseModel):
+#     username: str
+#     full_name: str | None = None
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     *,
+#     item_id: int = Path(..., title="The ID of the item to get", ge=0, le=150),
+#     q: str | None = None,
+#     item: Item = Body(..., embed=True),
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if item:
+#         results.update({"item": item})
+#     return results
+
+## Part 8 -> Body - Fields
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = Field(
+#         None, title="The description of the item", max_length=300
+#     )
+#     price: float = Field(..., gt=0, description="The price must be greater than zero.")
+#     tax: float | None = None
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item = Body(..., embed=True)):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+## Part 9 -> Body - Nested Models
+# class Image(BaseModel):
+#     url: HttpUrl
+#     name: str
+#
+#
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     tags: set[str] = []
+#     image: list[Image] | None = None
+#
+#
+# class Offer(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     items: list[Item]
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+#
+#
+# @app.post("/offers")
+# async def create_offer(offer: Offer = Body(..., embed=True)):
+#     return offer
+#
+#
+# @app.post("/images/multiple")
+# async def create_multiple_images(images: list[Image]):
+#     return images
+#
+#
+# @app.post("/blah")
+# async def create_some_blahs(blahs: dict[int, float]):
+#     return blahs
+
+## Part 10 - Declare Request Example Data
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#
+#     # class Config:
+#     #     schema_extra = {
+#     #         "example": {
+#     #             "name": "Foo",
+#     #             "description": "A very nice Item",
+#     #             "price": 16.25,
+#     #             "tax": 1.67,
+#     #         }
+#     #     }
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     item_id: int,
+#     item: Item = Body(
+#         ...,
+#         examples={
+#             "normal": {
+#                 "summary": "A normal example",
+#                 "description": "A __normal__ item works _correctly_",
+#                 "value": {
+#                     "name": "Foo",
+#                     "description": "A very nice Item",
+#                     "price": 16.25,
+#                     "tax": 1.67,
+#                 },
+#             },
+#             "converted": {
+#                 "summary": "An example with converted data",
+#                 "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
+#                 "value": {"name": "Bar", "price": "16.25"},
+#             },
+#             "invalid": {
+#                 "summary": "Invalid data is rejected with an error",
+#                 "description": "Hello youtubers",
+#                 "value": {"name": "Baz", "price": "sixteen point two five"},
+#             },
+#         },
+#     ),
+# ):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+## Part 11 - Extra Data Types
+# @app.put("/items/{item_id}")
+# async def read_items(
+#     item_id: UUID,
+#     start_date: datetime | None = Body(None),
+#     end_date: datetime | None = Body(None),
+#     repeat_at: time | None = Body(None),
+#     process_after: timedelta | None = Body(None),
+# ):
+#     start_process = start_date + process_after
+#     duration = end_date - start_process
+#     return {
+#         "item_id": item_id,
+#         "start_date": start_date,
+#         "end_date": end_date,
+#         "repeat_at": repeat_at,
+#         "process_after": process_after,
+#         "start_process": start_process,
+#         "duration": duration,
+#     }
+
+## Part 12 - Cookie and Header Parameters
+# @app.get("/items")
+# async def read_items(
+#     cookie_id: str | None = Cookie(None),
+#     accept_encoding: str | None = Header(None),
+#     sec_ch_ua: str | None = Header(None),
+#     user_agent: str | None = Header(None),
+#     x_token: list[str] | None = Header(None),
+# ):
+#     return {
+#         "cookie_id": cookie_id,
+#         "Accept-Encoding": accept_encoding,
+#         "sec-ch-ua": sec_ch_ua,
+#         "User-Agent": user_agent,
+#         "X-Token values": x_token,
+#     }
+
+## Part 13 - Response Model
 class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
-    tax: float | None = None
-    
-class User(BaseModel):
+    tax: float = 10.5
+    tags: list[str] = []
+
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+}
+
+
+@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+async def read_item(item_id: Literal["foo", "bar", "baz"]):
+    return items[item_id]
+
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+    return item
+
+
+class UserBase(BaseModel):
     username: str
+    email: EmailStr
     full_name: str | None = None
-    email: str | None = None
-    disabled: bool | None = None
 
 
-@app.put("/items/{item_id}")
-async def update_item(
-    *,
-    item_id: int = Path(..., title="The item_id of the item to get", gt=0, le=150),
-    q: str | None = None,
-    short: bool = False,
-    item: Item | None = None,
-    user: User,
-    importance: int = Body(..., ended=True),
-):
-    results = {"item_id": item_id}
-    if item:
-        results.update(item.dict())
-    if q:
-        results.update({"q": q})
-    if user:
-        results.update({"user": user})
-    if importance:
-        results.update({"importance": importance})
-    return results
+class UserIn(UserBase):
+    password: str
+
+
+class UserOut(UserBase):
+    pass
+
+
+@app.post("/user/", response_model=UserOut)
+async def create_user(user: UserIn):
+    return user
+
+
+@app.get(
+    "/items/{item_id}/name",
+    response_model=Item,
+    response_model_include={"name", "description"},
+)
+async def read_item_name(item_id: Literal["foo", "bar", "baz"]):
+    return items[item_id]
+
+
+@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
+async def read_items_public_data(item_id: Literal["foo", "bar", "baz"]):
+    return items[item_id]
