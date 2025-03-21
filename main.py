9a1129d48ab16a6cf1f0286455d4bd408dc5f70f
Dang Xuan Truong <truongdx@runsystem.net>
2025-03-21 09:32:22 +0700

    up

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from datetime import datetime, time, timedelta
 from enum import Enum
-from typing import Literal
+from typing import Literal, Union
 from uuid import UUID
 
-from fastapi import Body, FastAPI, Query, Path, Cookie, Header
+from fastapi import (
+    Body,
+    FastAPI,
+    Query,
+    Path,
+    Cookie,
+    Header,
+    status,
+    Form,
+    File,
+    UploadFile,
+)
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -335,59 +347,203 @@ app = FastAPI()
 #     }
 
 ## Part 13 - Response Model
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float = 10.5
-    tags: list[str] = []
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float = 10.5
+#     tags: list[str] = []
+#
+#
+# items = {
+#     "foo": {"name": "Foo", "price": 50.2},
+#     "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
+#     "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
+# }
+#
+#
+# @app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
+# async def read_item(item_id: Literal["foo", "bar", "baz"]):
+#     return items[item_id]
+#
+#
+# @app.post("/items/", response_model=Item)
+# async def create_item(item: Item):
+#     return item
+#
+#
+# class UserBase(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
+#
+#
+# class UserIn(UserBase):
+#     password: str
+#
+#
+# class UserOut(UserBase):
+#     pass
+#
+#
+# @app.post("/user/", response_model=UserOut)
+# async def create_user(user: UserIn):
+#     return user
+#
+#
+# @app.get(
+#     "/items/{item_id}/name",
+#     response_model=Item,
+#     response_model_include={"name", "description"},
+# )
+# async def read_item_name(item_id: Literal["foo", "bar", "baz"]):
+#     return items[item_id]
+#
+#
+# @app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
+# async def read_items_public_data(item_id: Literal["foo", "bar", "baz"]):
+#     return items[item_id]
+
+## Part 14 - Extra Models
+# class UserBase(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
+#
+#
+# class UserIn(UserBase):
+#     password: str
+#
+#
+# class UserOut(UserBase):
+#     pass
+#
+#
+# class UserInDB(UserBase):
+#     hashed_password: str
+#
+#
+# def fake_password_hasher(raw_password: str):
+#     return f"supersecret{raw_password}"
+#
+#
+# def fake_save_user(user_in: UserIn):
+#     hashed_password = fake_password_hasher(user_in.password)
+#     user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+#     print("User 'saved'.")
+#     return user_in_db
+#
+#
+# @app.post("/user/", response_model=UserOut)
+# async def create_user(user_in: UserIn):
+#     user_saved = fake_save_user(user_in)
+#     return user_saved
+#
+#
+# class BaseItem(BaseModel):
+#     description: str
+#     type: str
+#
+#
+# class CarItem(BaseItem):
+#     type = "car"
+#
+#
+# class PlaneItem(BaseItem):
+#     type = "plane"
+#     size: int
+#
+#
+# items = {
+#     "item1": {"description": "All my friends drive a low rider", "type": "car"},
+#     "item2": {
+#         "description": "Music is my aeroplane, it's my aeroplane",
+#         "type": "plane",
+#         "size": 5,
+#     },
+# }
+#
+#
+# @app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+# async def read_item(item_id: Literal["item1", "item2"]):
+#     return items[item_id]
+#
+#
+# class ListItem(BaseModel):
+#     name: str
+#     description: str
+#
+#
+# list_items = [
+#     {"name": "Foo", "description": "There comes my hero"},
+#     {"name": "Red", "description": "It's my aeroplane"},
+# ]
+#
+#
+# @app.get("/list_items/", response_model=list[ListItem])
+# async def read_items():
+#     return items
+#
+#
+# @app.get("/arbitrary", response_model=dict[str, float])
+# async def get_arbitrary():
+#     return {"foo": 1, "bar": "2"}
+
+## Part 15 - Response Status Codes
+# @app.post("/items/", status_code=status.HTTP_201_CREATED)
+# async def create_item(name: str):
+#     return {"name": name}
+#
+#
+# @app.delete("/items/{pk}", status_code=status.HTTP_204_NO_CONTENT)
+# async def delete_item(pk: str):
+#     print("pk", pk)
+#     return pk
+#
+#
+# @app.get("/items/", status_code=status.HTTP_302_FOUND)
+# async def read_items_redirect():
+#     return {"hello": "world"}
+
+## Part 16 - Form Fields
+# @app.post("/login/")
+# async def login(username: str = Form(...), password: str = Body(...)):
+#     print("password", password)
+#     return {"username": username}
+#
+#
+# @app.post("/login-json/")
+# async def login_json(username: str = Body(...), password: str = Body(...)):
+#     print("password", password)
+#     return {"username": username}
+
+## Part 17 - Request Files
+@app.post("/files/")
+async def create_file(
+    files: list[bytes] = File(..., description="A file read as bytes")
+):
+    return {"file_sizes": [len(file) for file in files]}
 
 
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
-}
+@app.post("/uploadfile/")
+async def create_upload_file(
+    files: list[UploadFile] = File(..., description="A file read as UploadFile")
+):
+    return {"filename": [file.filename for file in files]}
 
 
-@app.get("/items/{item_id}", response_model=Item, response_model_exclude_unset=True)
-async def read_item(item_id: Literal["foo", "bar", "baz"]):
-    return items[item_id]
-
-
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item):
-    return item
-
-
-class UserBase(BaseModel):
-    username: str
-    email: EmailStr
-    full_name: str | None = None
-
-
-class UserIn(UserBase):
-    password: str
-
-
-class UserOut(UserBase):
-    pass
-
-
-@app.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn):
-    return user
-
-
-@app.get(
-    "/items/{item_id}/name",
-    response_model=Item,
-    response_model_include={"name", "description"},
-)
-async def read_item_name(item_id: Literal["foo", "bar", "baz"]):
-    return items[item_id]
-
-
-@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
-async def read_items_public_data(item_id: Literal["foo", "bar", "baz"]):
-    return items[item_id]
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
